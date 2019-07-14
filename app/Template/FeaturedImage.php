8004da70e5949a280b43b3026cd3de94e47e9fab
Justin Tadlock <justintadlock@gmail.com>
2019-07-14 13:26:57 -0500

    Image adjustments.  Primarily code cleanup for the `FeaturedImage` class.

--- app/Template/FeaturedImage.php
+++ app/Template/FeaturedImage.php
@@ -17,6 +17,7 @@ namespace Exhale\Template;
 use Hybrid\App;
 use Hybrid\Carbon\Image;
 use Exhale\Image\Size\Sizes;
+use Exhale\Template\Loop;
 use Exhale\Tools\Mod;
 
 /**
@@ -57,16 +58,13 @@ class FeaturedImage extends Image {
 			$args['link'] = true;
 		}
 
-
 		$args['attr']['loading'] = 'auto';
 
 		if ( 0 < $GLOBALS['wp_query']->current_post ) {
 			$args['attr']['loading'] = 'lazy';
 		}
 
-		$l_type = is_home() ? 'blog' : 'archive';
-
-		$args['size']    = Mod::get( "loop_{$l_type}_image_size" );
+		$args['size']    = Loop::imageSize()->name();
 		$args['class']   = 'entry__image aligncenter';
 		$args['post_id'] = $post_id;
 		$args['before']  = sprintf( '<figure class="entry__media alignfull"%s>', $context );
@@ -75,15 +73,23 @@ class FeaturedImage extends Image {
 		return parent::carbon( $type, $args );
 	}
 
-
-       public static function display( $type, array $args = [] ) {
-	       echo static::render( $type, $args );
-       }
+	/**
+	 * Outputs the image HTML.
+	 *
+	 * @since  2.1.0
+	 * @access public
+	 * @param  array|string  $type
+	 * @param  array         $args
+	 * @return string
+	 */
+	public static function display( $type, array $args = [] ) {
+		echo static::render( $type, $args );
+	}
 
 	/**
 	 * Returns the image HTML.
 	 *
-	 * @since  1.0.0
+	 * @since  2.1.0
 	 * @access public
 	 * @param  array|string  $type
 	 * @param  array         $args
@@ -93,54 +99,61 @@ class FeaturedImage extends Image {
 
 		$image = static::image( $type, $args );
 
-		if ( $image ) {
-			return $image->render();
-		}
-
-		$l_type = is_home() ? 'blog' : 'archive';
-
-		$layout = App::resolve( 'layouts/loop' )->get( Mod::get( "loop_{$l_type}_layout" ) );
-
-		if ( is_home() || is_archive() && $layout->requiresImage() ) {
-			return static::svgFallback( $type, $args );
-		}
-
-		return '';
+		return $image ? $image->render() : static::svgFallback( $type, $args );
 	}
 
+	/**
+	 * SVG image fallback shown for layouts that require an image.
+	 *
+	 * @since  2.1.0
+	 * @access public
+	 * @param  array|string  $type
+	 * @param  array         $args
+	 * @return string
+	 */
 	private static function svgFallback( $type, array $args = [] ) {
 
-		$l_type = is_home() ? 'blog' : 'archive';
-
-		$size = App::resolve( Sizes::class )->get( Mod::get( "loop_{$l_type}_image_size" ) );
+		if ( ! ( is_home() || is_archive() ) || ! Loop::layout()->requiresImage() ) {
+			return '';
+		}
 
 		$post_id = ! empty( $args['post_id'] ) ? $args['post_id'] : get_the_ID();
+		$size    = Loop::imageSize();
 		$context = '';
 
-			// If we're viewing this in the customize preview frame, we need
-			// to add an attribute, which will pass our custom context back
-			// to the partial render method.
-			if ( is_customize_preview() ) {
-				$data = [ 'post_id' => $post_id ];
+		// If we're viewing this in the customize preview frame, we need
+		// to add an attribute, which will pass our custom context back
+		// to the partial render method.
+		if ( is_customize_preview() ) {
+			$data = [ 'post_id' => $post_id ];
 
-				$context = sprintf(
-					' data-customize-partial-placement-context="%s"',
-					esc_attr( wp_json_encode( $data ) )
-				);
-			}
+			$context = sprintf(
+				' data-customize-partial-placement-context="%s"',
+				esc_attr( wp_json_encode( $data ) )
+			);
+		}
 
-		return sprintf(
-			'<figure class="entry__media alignfull"%1$s><a href="%2$s">
-				<?xml version="1.0"?>
-				<svg class="entry__image aligncenter" fill="#%3$s" width="%4$s" height="%5$s" viewBox="0 0 %4$s %5$s">
-					<rect class="svg-shape" width="%4$s" height="%5$s" />
-				</svg>
-			</a></figure>',
-			$context,
-			esc_url( get_permalink( $args['post_id'] ) ),
-			sanitize_hex_color_no_hash( \Exhale\Tools\Mod::color( 'primary-link' ) ),
+		$svg = sprintf(
+			'<svg class="entry__image aligncenter" fill="#%1$s" width="%2$s" height="%3$s" viewBox="0 0 %2$s %3$s">
+				<rect class="svg-shape" width="%2$s" height="%3$s" />
+			</svg>',
+			sanitize_hex_color_no_hash( Mod::color( 'primary-link' ) ),
 			esc_attr( $size->width() ),
 			esc_attr( $size->height() )
+		);
+
+		if ( ! isset( $args['link'] ) || $args['link'] ) {
+			$svg = sprintf(
+				'<a href="%s">%s</a>',
+				esc_url( get_permalink( $args['post_id'] ) ),
+				$svg
+			);
+		}
+
+		return sprintf(
+			'<figure class="entry__media alignfull"%s>%s</figure>',
+			$context,
+			$svg
 		);
 	}
 }
